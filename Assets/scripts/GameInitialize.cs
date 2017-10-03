361e3299e6a3f4b95c3fe2e9ec2ced3cb21e98a9
li-tianfang <738843024@qq.com>
2017-10-04 00:10:17 +0800

    初始化背景和飞机

--- Assets/scripts/GameInitialize.cs
+++ Assets/scripts/GameInitialize.cs
@@ -4,24 +4,60 @@ using UnityEngine;
 
 public class GameInitialize : MonoBehaviour {
 	public GameObject gameBg,planeBody;
+	public int[,] gameMap;
+	public int[,] 
+	plane_1 = new int[4,5]{{0,0,1,0,0},{1,1,1,1,1},{0,0,1,0,0},{0,1,1,1,0}};
+//	public int[,] intArray = new int[2,3] {{1,2,3},{4,5,6}};
 
 	private int bgWidth = 9,bgHigh = 16;
 	// Use this for initialization
 	void Start () {
-		initialize ();
+		initializeBg ();
+		initializePlane ();
 	}
-	void initialize(){
+	void initializeBg(){
 //		GameObject go = gameObject.GetComponent<GameController>().gameBg;
-		for(int i=0;i<=bgWidth;i++){
-			for(int j=0;j<=bgHigh;j++){
-			GameObject bgCube = Instantiate (gameObject.GetComponent<GameInitialize>().gameBg,transform.Find("GameViewRoot"));
-			bgCube.name = "cube"+i+","+j;
-			float posX = 110f * i;
-			float posY = 110f * j;
-			bgCube.transform.position = new Vector3 (posX+50f,posY+50f,0);
+		gameMap = new int[bgWidth,bgHigh];
+		for (int x = 0; x < bgWidth; x++) {
+			for (int y = 0; y < bgHigh; y++) {
+				gameMap [x, y] = 0;
+
+			}
+		}
+		for(int i=0;i<bgWidth;i++){
+			for(int j=0;j<bgHigh;j++){
+				if (gameMap [i, j] == 0) {
+					GameObject bgCube = Instantiate (gameObject.GetComponent<GameInitialize> ().gameBg, transform.Find ("GameViewRoot"));
+					bgCube.name = "bgCube" + i + "," + j;
+					float posX = 110f * i;
+					float posY = 110f * j;
+					bgCube.transform.position = new Vector3 (posX + 50f, posY + 50f, 0);
+				}
 			
 			}
 		}
 
+	}
+	void initializePlane(){
+		for (int x = 0; x < plane_1.GetLength(0); x++) {
+			for (int y = 0; y < plane_1.GetLength(1); y++) {
+				gameMap [x, y] = plane_1[x,y];
+				Debug.Log (plane_1[x,y]);
+			}
+		}
+		for(int i=0;i<bgWidth;i++){
+			for(int j=0;j<bgHigh;j++){
+				if (gameMap [i, j] == 1) {
+//					GameObject.Destroy (gameObject);
+					GameObject bgCube = Instantiate (gameObject.GetComponent<GameInitialize> ().planeBody, transform.Find ("GameViewRoot"));
+					bgCube.name = "plane" + i + "," + j;
+					float posX = 110f * i;
+					float posY = 110f * j;
+					bgCube.transform.position = new Vector3 (posX + 50f, posY + 50f, 0);
+					Debug.Log (gameMap[i,j]);
+				}
+
+			}
+		}
 	}
 }
