750997a9671dad99cee7ba26fdc3d34e032814c5
Muyou <504661744@qq.com>
2017-12-01 17:46:49 +0800

    检测是否击中飞机或击空

---
+++ Assets/scripts/PlayerGun.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGun : MonoBehaviour {
+	public GameObject shootClick;
+	public void CheckShoot(){
+		if(shootClick.transform.tag == "Plane"){
+			Debug.Log ("shoot the plane!!!");
+			
+		}else if(shootClick.transform.tag == "BgMap"){
+			Debug.Log ("you shoot air...");
+		}
+	}
+}
