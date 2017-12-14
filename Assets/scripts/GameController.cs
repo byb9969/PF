41c8b9b72600030090755f301f62d03c268ff916
Muyou <504661744@qq.com>
2017-12-14 14:40:22 +0800

    test photon

--- Assets/scripts/GameController.cs
+++ Assets/scripts/GameController.cs
@@ -11,23 +11,9 @@ public class GameController : MonoBehaviour
 	{
 		
 	}
-
-	public void EnemyAttackBtn ()
-	{
-		for (int i = 0; i < transform.GetComponent<GameInitialize> ().bgWidth; i++) {
-			for (int j = 0; j < transform.GetComponent<GameInitialize> ().bgHigh; j++) {
-				if (i == enemyAttack_x && j == enemyAttack_y) {
-					float posX = 110f * i+50f;
-					float posY = 110f * j+50f;
-					Debug.Log (posX+">>>" + posY);
-					Instantiate (gameObject.GetComponent<GameInitialize> ().attackAnima,new Vector3(posX,posY,0),transform.rotation,transform.Find ("GameViewRoot"));
-
-
-				}
-			}
-		}
+	public void FireBtn(){
+		
 	}
-	// Update is called once per frame
 	void Update ()
 	{
 	}
--- Assets/scripts/PlayerGun.cs
+++ Assets/scripts/PlayerGun.cs
@@ -8,10 +8,28 @@ public class PlayerGun : MonoBehaviour {
 	public bool cubeKind;
 	 void CheckShoot(){
 		if (cubeKind) {
-			
+			EnemyAttackBtn ();
 			Debug.Log ("击中!" + mapPosx + "," + mapPosy + "-->" + cubeKind);
 		} else {
+			EnemyAttackBtn ();
 			Debug.Log ("打空!!" + mapPosx + "," + mapPosy + "-->" + cubeKind);
 		}
 	}
+	void EnemyAttackBtn ()
+	{
+		for (int i = 0; i < transform.GetComponent<GameInitialize> ().bgWidth; i++) {
+			for (int j = 0; j < transform.GetComponent<GameInitialize> ().bgHigh; j++) {
+				if (i == mapPosx && j == mapPosy) {
+					float posX = 110f * i+50f;
+					float posY = 110f * j+50f;
+					Debug.Log (posX+">>>" + posY);
+					Instantiate (gameObject.GetComponent<GameInitialize> ().attackAnima,new Vector3(posX,posY,0),transform.rotation,transform.Find ("GameViewRoot"));
+
+
+				}
+			}
+		}
+	}
+
+
 }
